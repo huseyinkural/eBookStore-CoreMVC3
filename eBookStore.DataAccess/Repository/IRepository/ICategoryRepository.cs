0bedb080a4849db483d8f0b6dd4ea52a0a05980a
huseyinkural <huseyinkural35@gmail.com>
2020-07-09 21:28:07 +0300

    Implemented CategoryRepository and IRepository.

---
+++ eBookStore.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using eBookStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eBookStore.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository : IRepository<Category>
+    {
+        void Update(Category category);
+    }
+}
