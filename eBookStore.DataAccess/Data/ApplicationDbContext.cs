e6e8ad842012b8c09515d8250f2954cac5657723
huseyinkural <huseyinkural35@gmail.com>
2020-07-11 16:02:40 +0300

    CoverType is added to Db, Model, IRepository, Repository and UnitOfWork.

--- eBookStore.DataAccess/Data/ApplicationDbContext.cs
+++ eBookStore.DataAccess/Data/ApplicationDbContext.cs
@@ -15,5 +15,6 @@ namespace eBookStore.DataAccess.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<CoverType> CoverTypes { get; set; }
     }
 }
--- eBookStore.DataAccess/Migrations/20200711125942_addCoverTypeToDb.Designer.cs
+++ eBookStore.DataAccess/Migrations/20200711125942_addCoverTypeToDb.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using eBookStore.DataAccess.Data;
 
 namespace eBookStore.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200711125942_addCoverTypeToDb")]
+    partial class addCoverTypeToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
