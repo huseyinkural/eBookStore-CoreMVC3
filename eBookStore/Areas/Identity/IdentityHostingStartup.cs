95f8199d4d45efb7492d2e72b006acf50e1c500b
huseyinkural <huseyinkural35@gmail.com>
2020-07-25 00:25:21 +0300

    Identity classes are added.

---
+++ eBookStore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using eBookStore.DataAccess.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(eBookStore.Areas.Identity.IdentityHostingStartup))]
+namespace eBookStore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
