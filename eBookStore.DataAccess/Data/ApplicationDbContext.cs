059476e45f4180aea531808e691ea94cc1b5503b
huseyinkural <huseyinkural35@gmail.com>
2020-07-24 19:47:41 +0300

    ApplicationUser class is added and application user properties are added to db

--- eBookStore.DataAccess/Data/ApplicationDbContext.cs
+++ eBookStore.DataAccess/Data/ApplicationDbContext.cs
@@ -16,7 +16,7 @@ namespace eBookStore.DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
-
         public DbSet<Product> Products { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 }
